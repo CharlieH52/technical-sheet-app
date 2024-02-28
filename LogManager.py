1e0da21ee0130667d40e90e10a20e4c21cf7b217
CharlieH52 <carlos.gomez.h52@gmail.com>
2024-02-28 12:33:33 -0600

    Se soluciono el problema de atributos en LogManager.py

--- LogManager.py
+++ LogManager.py
@@ -5,8 +5,8 @@ from platform import node
 
 class LogManagerSystem:
     def __init__(self):
-        self.op_log_directory = self.output_route()
         self.folder_name = 'LOGS'
+        self.op_log_directory = self.output_route()
         
     def output_route(self):
         logs_directory = os.path.join(os.getcwd(), self.folder_name)
@@ -14,7 +14,7 @@ class LogManagerSystem:
 
     def error_logs_print(self, directory, log):
         date = str(time.strftime('%d-%m-%Y_%H-%M-%S'))
-        log_name = f'E_LOG-{node}-{date}.txt'
+        log_name = f'E_LOG-{node}-{date}.log'
         new_directory = os.path.join(directory, log_name)       
         with open(new_directory, 'w') as file:
             file.write(log)
