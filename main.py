d3463d8ee0194a6525bce5d54422cbbe20f97373
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-05-11 20:55:55 -0600

    [DEL] Old comments and outdate methods and structure

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from src.google_connect.google_actions import GoogleSheet
 from src.device_information.anydesk_info import AnyDeskInfo
 from src.device_information.hardware_info import HardwareInfo
@@ -8,30 +6,21 @@ from src.device_information.system_info import SystemInfo
 from src.data_management import Writer
 from src.builder import ComputerBuilder
 
-file_name_gs = 'service_account.json'
-google_sheet = 'hoja-tec'
-sheet_name = 'db_tec'
+# Do you need use the API?
+SHEET_FUNCTION = False
 
-if __name__ == '__main__':
+# Google Sheet Config Lines
+file_name_gs = "service_account.json"
+google_sheet = "hoja-tec"
+sheet_name = "db_tec"
+
+if __name__ == "__main__":
     wr = Writer()
     data_device = ComputerBuilder(anydesk_provider=AnyDeskInfo(), hardware_provider=HardwareInfo(), system_provider=SystemInfo(), network_provider=NetworkInfo())
     new_data = data_device.get_all_data()
-    wr.update_local_storage(new_data.to_dictionary())
-    google = GoogleSheet(file_name_gs, google_sheet, sheet_name)
     
-    # Se debe evaluar la conexión a internet para discernir si se envian los datos
-    # mediante la API de google o si se crea un archivo script para su ejeccución
-    # posterior de 1 sola ejecución.
+    wr.find_and_update_by_mac(new_data.to_dictionary())
 
-    def generate_uid():
-        # Genera un ID leyendo la cantidad de filas habidas en la hoja de google
-        # a partir de la fila de encabezados A1.
-        pass
-
-    # uid = generate_uid()
-    # values = dictionary
-
-    # Se pasan el ID y los datos en formato de diccionario o lista de lista para
-    # la escritura del nuevo equipo en la hoja.
-    # google.upload_profile(uid, values)
+    if SHEET_FUNCTION == True:
+        google = GoogleSheet(file_name_gs, google_sheet, sheet_name)
     
