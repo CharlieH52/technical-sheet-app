6e6fda447b9b140e5f5e25b1f73371cd1f231b9e
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-05-11 20:49:21 -0600

    [REF] Class structure and single responsibility principle implemented

--- src/device_information/anydesk_info.py
+++ src/device_information/anydesk_info.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 from os import getlogin
 
@@ -9,38 +8,40 @@ ANYDESK_ERRORS = {
     'READ_ID_ERROR': "No se encuentra el ID dentro del archivo. Reinstala Anydesk"
 }
 
-user = getlogin()
-
 class AnyDeskInfo:
     def __init__(self):
-        self.anydesk_path = f'C:/Users/{user}/AppData/Roaming/AnyDesk'
-        self.config_file = f'C:/Users/{user}/AppData/Roaming/AnyDesk/system.conf'
+        self.user = getlogin()
+        self.id_key = "ad.anynet.id"
+        self.anydesk_path = f"C:/Users/{self.user}/AppData/Roaming/AnyDesk"
+        self.config_file = f"C:/Users/{self.user}/AppData/Roaming/AnyDesk/system.conf"
 
-    # Get the desktop ID of AnyDesk.
+    # Read .config file and return a dictionary.
+    def read_config_file(self, file_path):
+        config = {}
+        with open(file_path, "r") as file:
+            for line in file:
+                key, value = line.strip().split('=', 1)
+                config[key] = value
+        return config
+
+    # Verify the DEFAULT installation route.
+    def verify_base_files(self):
+        try:
+            if os.path.exists(self.anydesk_path):
+                try:
+                    if os.path.exists(self.config_file):
+                        ids = self.read_config_file(self.config_file)
+                        if self.id_key in ids:
+                            return True
+                except OSError:
+                    print(ANYDESK_ERRORS['READ_CONF_ERROR'])
+            return False
+        except OSError:
+            print(ANYDESK_ERRORS['MAIN_PATH'])
+
+    # Orchestrator
     def get_anydesk_desktop_id(self):
-        # Verify the NORMAL installation route and the .conf file with the ID.
-        def _files_checker():
-            try:
-                if not os.path.exists(self.anydesk_path):
-                    return "AnyDesk is not installed or the installation directory is different."
-            except OSError:
-                print(ANYDESK_ERRORS['MAIN_PATH'])
-
-            try:
-                if not os.path.exists(self.config_file):
-                    return "Type the ID manually."
-            except OSError:
-                print(ANYDESK_ERRORS['READ_CONF_ERROR'])
-            
-            return None
-                    
-        def _read_id():
-            regEx = r".id=([0-9]*)"
-            with open(self.config_file, 'r') as file:
-                for line in file:
-                    anyID = re.search(regEx, line)
-                    if anyID:
-                        return anyID.group(1)
-        
-        if _files_checker() == None:
-            return _read_id()
+        verify = self.verify_base_files()
+        if verify: 
+            config_file = self.read_config_file(self.config_file)
+            return config_file[self.id_key]
