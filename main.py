f45fb2797293540172392065bf3ba52c96cee9bd
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-06-06 13:16:00 -0600

    Deleted API configuration lines and the imports are fixed

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-from src.modules.anydesk import AnyDeskInfo
-from src.modules.hardware_info import HardwareInfo
-from src.modules.network_info import NetworkInfo
-from src.modules.system_info import SystemInfo
+from modules.mod_anydesk import AnyDeskInfo
+from modules.mod_hardware import HardwareInfo
+from modules.mod_network import NetworkInfo
+from modules.mod_os import OperativeSystem
+from modules.mod_user import UserInformation 
 from src.data_management import Writer
 from src.builder import ComputerBuilder
 import requests
 
 # Do you need use the API?
-SHEET_FUNCTION = True
-
-# Google Sheet Config Lines
-file_name_gs = "service_account.json"
-google_sheet = "hoja-tec"
-sheet_name = "db_tec"
+SHEET_FUNCTION = False
 
 if __name__ == "__main__":
     wr = Writer()
-    data_device = ComputerBuilder(anydesk_provider=AnyDeskInfo(), hardware_provider=HardwareInfo(), system_provider=SystemInfo(), network_provider=NetworkInfo())
+    data_device = ComputerBuilder(anydesk_provider=AnyDeskInfo(), user_provider=UserInformation(), hardware_provider=HardwareInfo(), system_provider=OperativeSystem(), network_provider=NetworkInfo())
     new_data = data_device.get_all_data()
     
     wr.find_and_update_by_mac(new_data.to_dictionary())
@@ -25,5 +21,3 @@ if __name__ == "__main__":
         endpoint = "http://127.0.0.1:8000/post"
         inc_data = new_data.to_dictionary()
         response = requests.post(url=endpoint, json=inc_data)
-
-    
