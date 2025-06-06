bc5585b4fbee61db3900ea01ec8e4d1f0f874d7c
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-06-06 13:13:45 -0600

    Added user to the constructor and user_name has been implemented in get_all_data

--- src/builder.py
+++ src/builder.py
@@ -1,18 +1,19 @@
 from platform import node
 
-from object.computer_object import Computer
+from src.object.computer_object import Computer
 
 class ComputerBuilder:
-    def __init__(self, network_provider, hardware_provider, system_provider, anydesk_provider):
+    def __init__(self, network_provider, user_provider, hardware_provider, system_provider, anydesk_provider):
         self.network = network_provider
         self.hardware = hardware_provider
         self.system = system_provider
         self.anydesk = anydesk_provider
+        self.user = user_provider
 
     def get_all_data(self) -> Computer:
         self.network.get_info()
         return Computer(device_name = node(),
-                       user_name = "String incoming -> Pending",
+                       user_name = self.user.get_full_name(),
                        machine_mac = self.network.network_info['MAC'],
                        machine_ip = self.network.network_info['IP4'],
                        mobo_mark = self.hardware.get_motherboard_manufacturer(),
