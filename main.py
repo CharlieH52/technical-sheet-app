144a41b92d2a0ec277d36ac304ff4b6e38336008
Charlie Chan <carlos.gomez.h52@gmail.com>
2025-08-29 02:42:01 -0600

    Refactor move API connection logic to connector module

--- main.py
+++ main.py
@@ -5,23 +5,21 @@ from src.modules.mod_os import OperativeSystem
 from src.modules.mod_user import UserInformation 
 from src.data_management import Writer
 from src.builder import ComputerBuilder
-from src.pop_up import PopUp
-import requests
+# from src.connector import ApiClient
+from src.notifications.popup import PopUp
 
 # Do you need use the API?
-SHEET_FUNCTION = True
+# SHEET_FUNCTION = True
 
 if __name__ == "__main__":
     wr = Writer()
     data_device = ComputerBuilder(anydesk_provider=AnyDeskInfo(), user_provider=UserInformation(), hardware_provider=HardwareInfo(), system_provider=OperativeSystem(), network_provider=NetworkInfo())
-    new_data = data_device.get_all_data()
+    computer = data_device.create_computer()
+    computer_data = computer.to_dictionary()
 
-    wr.find_and_update_by_mac(new_data.to_dictionary())
+    wr.find_and_update_by_mac(computer_data)
 
     PopUp("Estado", "Ficha guardada correctamente.")
 
-    if SHEET_FUNCTION == True:
-        endpoint = "http://127.0.0.1:8000/post"
-        inc_data = new_data.to_dictionary()
-        response = requests.post(url=endpoint, json=inc_data)
-
+    # if SHEET_FUNCTION == True:
+    #     ApiClient(computer_data).post_computer_data(computer_data)
