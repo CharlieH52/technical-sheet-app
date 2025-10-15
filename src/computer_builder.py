71b35815bff3389fa0806195c1d5f68bb97d5aa8
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-10-15 11:01:11 -0600

    refactor: rename builder to computer_builder
    coment all lines for timestamp data

--- src/computer_builder.py
+++ src/computer_builder.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from models.computer import Computer
+# from datetime import datetime
 from platform import node
 
-from src.object.computer_object import Computer
 
 class ComputerBuilder:
     def __init__(self, network_provider, user_provider, hardware_provider, system_provider, anydesk_provider):
@@ -10,10 +10,10 @@ class ComputerBuilder:
         self.system = system_provider
         self.anydesk = anydesk_provider
         self.user = user_provider
-        self.current_time = self.__generate_time_stamp()
+    #     self.current_time = self.__generate_time_stamp()
 
-    def __generate_time_stamp(self) -> str:
-        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # def __generate_time_stamp(self) -> str:
+    #     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     def create_computer(self) -> Computer:
         self.network.get_info()
@@ -27,6 +27,6 @@ class ComputerBuilder:
                         operative_system = self.system.get_windows_version(),
                         storage = self.hardware.get_main_storage(),
                         memory_cap = self.hardware.get_total_memory_ram(),
-                        anydesk_id = self.anydesk.get_anydesk_desktop_id(),
-                        time_stamp= self.current_time
+                        anydesk_id = self.anydesk.get_anydesk_desktop_id()
+                        # time_stamp= self.current_time
                         )
