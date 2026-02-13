6a660ea42d3f44d335316a48c6ddd7f0bfa5dcd9
CharlieH52 <carlos.gomez.h52@gmail.com>
2026-02-13 14:39:08 -0600

    ref: wmic processor changed to cimclasses with powershell

--- src/modules/mod_hardware.py
+++ src/modules/mod_hardware.py
@@ -2,14 +2,13 @@ from src.command_processor import CommandProcessor
 from psutil import disk_usage, virtual_memory
 
 class HardwareInfo:
-    GET_CPU_DATA = "wmic cpu get name /format:list"
-    GET_MOBO_DATA = "wmic baseboard get product, manufacturer /format:list"
-    GET_MAIN_STORAGE_DEVICE = "wmic diskdrive where index=0 get model /format:list"
-
     def __init__(self):
-        self.cpu_data = CommandProcessor(self.GET_CPU_DATA).get_parsed_output()
-        self.mobo_data = CommandProcessor(self.GET_MOBO_DATA).get_parsed_output()
-        self.storage_data = CommandProcessor(self.GET_MAIN_STORAGE_DEVICE).get_parsed_output()
+        self.class_processor = "Win32_Processor"
+        self.class_baseboard = "Win32_BaseBoard"
+        self.class_diskdrive = "Win32_DiskDrive"
+        self.cpu = CommandProcessor(self.class_processor).get_parsed_output()
+        self.baseboard = CommandProcessor(self.class_baseboard).get_parsed_output()
+        self.diskdrive = CommandProcessor(self.class_diskdrive).get_parsed_output()
 
     # bits to bytes converter.
     def _bytes_converter(self, total_Bytes):
@@ -18,36 +17,36 @@ class HardwareInfo:
     # CPU
     # Obtain the full name and the main specs about the processor.
     def get_cpu_model(self):
-        return self.cpu_data['Name']
+        return self.cpu.get("Name")
     
     # MOTHERBOARD
     # Obtain the model name. 
     def get_motherboard_model(self):
-        return self.mobo_data['Product']
+        return self.baseboard.get("Product")
 
     # Obtain the name of the manufacturer.
     def get_motherboard_manufacturer(self):
-        return self.mobo_data['Manufacturer']
+        return self.baseboard.get("Manufacturer")
 
     # MEMORY RAM
     # Obtain the memory cap of the installed memory.
     def get_total_memory_ram(self):
         totalMemory = virtual_memory().total
-        systemMemory = f"{round(self._bytes_converter(totalMemory), 0)} GB"
+        systemMemory = f"{round(self._bytes_converter(totalMemory), 0)}"
         return systemMemory
     
     # MAIN STORAGE
     # Obtain the model name of the main storage device.
-    def _get_disk_model(self):
-        return self.storage_data['Model']
+    def get_disk_model(self):
+        return self.diskdrive.get("Model")
 
     # Obtain the memory cap of the main storage device.
-    def _get_storage_cap(self):
+    def get_storage_cap(self):
         totalStorage = disk_usage("C:\\").total
-        systemStorage = f"{round(self._bytes_converter(totalStorage), 0)} GB"
+        systemStorage = f"{round(self._bytes_converter(totalStorage), 0)}"
         return systemStorage
     
     # Get the full data about the main storage device.
     def get_main_storage(self):
-        return f"{self._get_storage_cap()} {self._get_disk_model()}"
+        return f"{self.get_storage_cap()} {self.get_disk_model()}"
     
--- src/modules/mod_os.py
+++ src/modules/mod_os.py
@@ -1,18 +1,18 @@
 from src.command_processor import CommandProcessor
 
 class OperativeSystem:
-    GET_OS_DATA = "wmic os get caption, osarchitecture /format:list"
 
     def __init__(self):
-        self.os_data = CommandProcessor(self.GET_OS_DATA).get_parsed_output()
+        self.class_computersystem = "Win32_OperatingSystem"
+        self.get_os_data = CommandProcessor(self.class_computersystem).get_parsed_output()
 
     # Gets the Windows product name.
     def win_product(self):
-        return self.os_data['Caption']
+        return self.get_os_data.get("Caption")
     
     # Gets the architecture bits of the OS.
     def win_architecture(self):
-        return self.os_data['OSArchitecture']
+        return self.get_os_data.get("OSArchitecture")
     
     def get_windows_version(self):
         concat_win_product = f"{self.win_product()} {self.win_architecture()}" 
